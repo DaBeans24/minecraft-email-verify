13de26952ecff8a95a5e5af8d4b3f7ffcdcc1e83
Stephen Baird <stephen.baird.ii@gmail.com>
2025-06-30 17:02:12 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+# Replace with your actual values
+DATABASE_CONFIG = {
+    'host': 'mysql.apexhosting.gdn',
+    'user': 'apexMC2826169',
+    'password': 'DAXnoO^Pwf4Lvn4TNQbBLQsBDAXnoO^Pwf4Lvn4TNQbBLQsB',
+    'database': 'apexMC2826169'
+}
+
+EMAIL_CONFIG = {
+    'smtp_server': 'smtp.gmail.com',
+    'smtp_port': 587,
+    'email': 'stephen.baird.ii@gmail.com',
+    'password': 'rbed ksvq kckz cntz'  # Use app password or secure method
+}
